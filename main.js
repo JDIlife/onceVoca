3753836a97ae122d98ae345e32aaa98c65fcb42a
baro-baro <lowlie98@naver.com>
2022-08-21 23:10:25 +0900

    feat: api에서 검색한 단어 데이터 중 필요한 데이터만 골라 별도의 배열에 저장

--- main.js
+++ main.js
@@ -70,24 +70,49 @@ let getWordData = async () => {
 
 let filterWordData = (dataStore) => {
 	let dataLength = dataStore.length
+
+	let wordData = [];
+
 	for(let i = 0; i < dataLength; i++){
 		let data = dataStore[i][0];
 		let meaningsLength = data.meanings.length;
-		let dataWord = data.word;
-		console.log("dataWord = ", dataWord);
+		let wordSpell = data.word;
+
+		let wordMean = [];
+
+		let wordObj = {};
+		wordObj.wordSpell = wordSpell;
+		wordObj.wordMean = wordMean;
+		wordData.push(wordObj);
+
 		for(let i = 0; i < meaningsLength; i++){
 			let meaning = data.meanings[i];
 			let partOfSpeech = meaning.partOfSpeech;
 			let defLength = meaning.definitions.length;
+
+			let defs = [];
+
+			let mean = {};
+			mean.partOfSpeech = partOfSpeech;
+			mean.defs = defs;
+			wordMean.push(mean);
+
+
 			for(let i = 0; i < defLength; i++){
 				let def = meaning.definitions[i].definition;
 				let example = meaning.definitions[i].example;
-				console.log("def = ", def)
-				console.log("ex = ", example);
+				let defEx = {};
+				defEx.def = def;
+				defEx.example = example;
+				defs.push(defEx);
 			}
-			console.log("part of speech = ", partOfSpeech)
+			//console.log("part of speech = ", partOfSpeech)
+			//console.log("array defs contain object = ", defs)
+			//console.log("mean = ", mean)
 		}
 	}
+
+	console.log("wordData = ", wordData);
 }
 
 searchBtn.addEventListener('click', createWordList);
