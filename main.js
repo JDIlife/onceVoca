85e120c495750470862c9469eb17572151491aa4
baro-baro <lowlie98@naver.com>
2022-08-21 18:12:21 +0900

    feat: api에서 검색한 단어 데이터 중 필요한 데이터를 선별, 단어들을 임시로 모아놓는 기능의 방법 수정

--- main.js
+++ main.js
@@ -11,29 +11,87 @@ const searchBtn = document.getElementById("searchBtn");
 
 // push the word with enter and wordInputBtn
 
-let wordList = [];
-
 let pushWord = () => {
 	if(searchInput.value != ""){
 		let text = searchInput.value;
-		wordList.push(text);
+		wordStore.innerHTML += `
+			<span>
+				${text}
+				<button class="deleteWord">
+					<i class="fa-solid fa-delete-left"></i>
+				</button>
+			</span>
+			`;
 	}
-	return wordList;
 }
 
 searchInput.addEventListener('keypress', (e) => {
 	if(e.key == "Enter"){
-		let wordList = pushWord(e);
-		console.log("word list = ", wordList)
+		pushWord();
 	}
 })
 
-wordInputBtn.addEventListener('click', (e) => {
-	let wordList = pushWord();
-	console.log("word list with click = ", wordList)
-})
+wordInputBtn.addEventListener('click', pushWord);
 
 // create temporary word store
+
+let createTempWord = (wordList) => {
+	for(let i = 0; i < wordList.length; i++){
+		console.log("list", wordList[i])
+	}
+	console.log("length = ", wordList.length)
+}
+
+//createTempWord(wordList);
+
+let wordList = [];
+
+let createWordList = () => {
+	
+	let length = wordStore.childElementCount
+	for(let i = 0; i < length; i++){
+		let word = wordStore.children[i].innerText;
+		wordList.push(word);
+		console.log("children[i] = ", wordStore.children[i].innerText)
+	}
+	console.log("word list = ", wordList);
+}
+
+let getWordData = async () => {
+	let dataStore = [];
+	for(let i = 0; i < wordList.length; i++){
+		let word = wordList[i];
+		let data = await getData(word);
+		dataStore.push(data);
+	}
+	console.log("dataStore = ", dataStore);
+	filterWordData(dataStore);
+}
+
+let filterWordData = (dataStore) => {
+	let dataLength = dataStore.length
+	for(let i = 0; i < dataLength; i++){
+		let data = dataStore[i][0];
+		let meaningsLength = data.meanings.length;
+		let dataWord = data.word;
+		console.log("dataWord = ", dataWord);
+		for(let i = 0; i < meaningsLength; i++){
+			let meaning = data.meanings[i];
+			let partOfSpeech = meaning.partOfSpeech;
+			let defLength = meaning.definitions.length;
+			for(let i = 0; i < defLength; i++){
+				let def = meaning.definitions[i].definition;
+				let example = meaning.definitions[i].example;
+				console.log("def = ", def)
+				console.log("ex = ", example);
+			}
+			console.log("part of speech = ", partOfSpeech)
+		}
+	}
+}
+
+searchBtn.addEventListener('click', createWordList);
+searchBtn.addEventListener('click', getWordData);
 
 // selecte the folder to store
 
@@ -46,31 +104,14 @@ wordInputBtn.addEventListener('click', (e) => {
 
 const getData = async (word) => {
 	let response = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
-	let output;
+
+
 	if(response.ok) {
 		const data = await response.json()
 		console.log("success")
-		console.log("data = ", data)
-		console.log("data[0].meanings = ", data[0].meanings)
-		console.log("data[0].meanings[0].defs... = ", data[0].meanings[0].definitions[0].definition)
-		console.log("data.length", data.length)
-		console.log("data[0].meanings.length", data[0].meanings.length)
-		console.log("data[0].meanings[0].length", data[0].meanings[0].length)
-		data.forEach(function(word){
-			output = `
-				<ul>
-					<li>${word.meanings[0].definitions[0].definition}</li>
-					<li>${word.meanings[0].definitions[0].example}</li>
-					<li>${word.meanings[1].definitions[0].definition}</li>
-				</ul>
-			`
-		})
-		wordStore.innerHTML = output;
+		return(data);
 	} else {
 		console.log("error")
 	}
 }
 
-let word = "component"
-
-getData(word);
